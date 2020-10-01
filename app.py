5e4211e7f0ec5b0ebb5270135fd0aadb944db7de
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-01 22:32:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
         context = request.args["context"]
         question = request.args["question"]
 
-        answer = rs.predict(contexxt, answer)
+        answer = rs.predict(context, answer)
         print(answer)
 
         return flask.render_template('index.html', question=question, answer=answer)
