59bcd7764457d848998eff4ec0e4189cb49f552f
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-02 11:02:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import time
 app = flask.Flask(__name__)
 
 
-@app.route('/')
+'''@app.route('/')
 def index():
 
     if request.args:
@@ -22,7 +22,49 @@ def index():
 
         return flask.render_template('index.html', question=question, answer=answer)
     else:
-        return flask.render_template('index.html')
+        return flask.render_template('index.html')'''
+
+
+# geting and sending response to dialogflow
+@app.route('/webhook', methods=['POST'])
+@cross_origin()
+def webhook():
+
+    req = request.get_json(silent=True, force=True)
+
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    #print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+# processing the request from dialogflow
+def processRequest(req):
+
+    #sessionID=req.get('responseId')
+    result = req.get("queryResult")
+    #user_says=result.get("queryText")
+    #log.write_log(sessionID, "User Says: "+user_says)
+    parameters = result.get("parameters")
+    context=parameters.get("context")
+    question = parameters.get("question")
+	 
+    intent = result.get("intent").get('displayName')
+    
+    if (intent=='QA - yes'):
+        prediction = rs.predict(context, question)
+       
+        fulfillmentText= "The Iris type seems to be..  {} !".format(prediction)
+        #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
+        return {
+            "fulfillmentText": fulfillmentText
+        }
 
 
 if __name__ == "__main__":
