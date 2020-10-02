b2dd8ab0266c0640293549b59a839026a3fa8378
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-02 21:22:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,27 @@ app = Flask(__name__)
 def hello():
     return 'Hello World'
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json(silent=True)
+    result = req.get("queryResult")
+    #user_says=result.get("queryText")
+    #log.write_log(sessionID, "User Says: "+user_says)
+    parameters = result.get("parameters")
+    context=parameters.get("context")
+    question = parameters.get("question")
+
+    
+    if (intent=='QA - yes'):
+        prediction = rs.predict(context,question)
+    
+       
+        fulfillmentText= "The Iris type seems to be..  {} !".format(prediction)
+        #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
+        return {
+            "fulfillmentText": fulfillmentText
+        }
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
