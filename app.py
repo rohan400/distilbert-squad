27437dd35fd9bf983771affe335e41ec813c66a0
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-03 00:32:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ celery.conf.update(app.config)
 
 @app.route('/')
 def run_jobs():
-    task = my_background_task.delay()
+    task = my_background_task.apply_async(countdown=20)
     return task
 
 @celery.task
@@ -47,6 +47,7 @@ def my_background_task():
     # some long running task here
     context = "Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei"
     question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
+    rs.load_model()
     answer = rs.predict(context, question)
     print(answer)
     return answer
