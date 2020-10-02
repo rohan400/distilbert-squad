dc7b42a2a911350346d42a8a13b0d567003ea0ef
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-02 22:10:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 import numpy as np
 from flask import Flask, request, make_response
-import json
-import pickle
 from flask_cors import cross_origin
 import rs
+import threading
+
+
+
+class threadClass:
+
+    def __init__(self):
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                       # Daemonize thread
+        thread.start()                             # Start the execution
+
+    def run(self):
+
+         #
+         # This might take several minutes to complete
+         someHeavyFunction()
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route('/', methods=['POST'])
+    try:
+        begin = threadClass()
+    except:
+        abort(500)
+
+    return "Task is in progress"
+
+
+
+def someHeavyFunction():
     context = "Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei"
     question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
 
