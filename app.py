30697a4610e54de9e22cb67351ef8e389a30064a
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-03 00:05:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask, request, make_response
 from flask_cors import cross_origin
 import rs
-import threading
+'''import threading
 
 
 from concurrent.futures import ThreadPoolExecutor
@@ -26,7 +26,31 @@ def someHeavyFunction():
     question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
     answer = rs.predict(context, question)
     print(answer)
+    return answer'''
+
+from celery import Celery
+
+app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+@app.route('/')
+def run_jobs():
+    task = my_background_task.apply_async(countdown=60)
+    return task
+
+@celery.task
+def my_background_task():
+    # some long running task here
+    context = "Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei"
+    question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
+    answer = rs.predict(context, question)
+    print(answer)
     return answer
+
 
 
 
