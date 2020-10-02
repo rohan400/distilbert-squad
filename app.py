bdac6680d9b4bc6f9a86b1c2ff39eebd14df7ce3
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-02 22:22:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,6 @@ app = Flask(__name__)
 
     return "Task is in progress"
 
-
-
 def someHeavyFunction():
     context = "Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei"
     question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
