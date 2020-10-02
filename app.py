1ce060ce5c351cb7ff64be94417a232ae928aafd
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-02 23:15:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import threading
 from concurrent.futures import ThreadPoolExecutor
 from time import sleep
 # DOCS https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
-executor = ThreadPoolExecutor(2)
+executor = ThreadPoolExecutor(1)
 
 app = Flask(__name__)
 
@@ -24,7 +24,6 @@ def someHeavyFunction():
     context = "Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei"
     question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
     answer = rs.predict(context, question)
-    sleep(10)
     print(answer)
     return answer
 
