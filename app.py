74ac90a426c344821b68ba228041580287145f76
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-02 22:44:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,34 +5,25 @@ import rs
 import threading
 
 
-
-class threadClass:
-
-    def __init__(self):
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True                       # Daemonize thread
-        thread.start()                             # Start the execution
-
-    def run(self):
-
-         #
-         # This might take several minutes to complete
-         someHeavyFunction()
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+# DOCS https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
+executor = ThreadPoolExecutor(2)
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-try:
-    begin = threadClass()
-except:
-    abort(500)
 
-    return "Task is in progress"
+@app.route('/')
+def run_jobs():
+    executor.submit(someHeavyFunction)
+    return 'One job were launched in background!'
+
+
 
 def someHeavyFunction():
     context = "Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei"
     question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
-
+    sleep(10)
     answer = rs.predict(context, question)
     print(answer)
     return answer
