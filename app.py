f1773dc4a2221e358d05befadd9fc17a0518d808
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-02 17:25:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,16 @@ import rs
 import flask
 import os
 import time
-from threading import Thread
+import logging
 
 
 app = flask.Flask(__name__)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
 
 
 @app.route('/')
-def hello():
-    context ='Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei'
-    question = 'Ce surprinde Macro Cam la Samsung Galaxy A31?'
-    answer = rs.predict(context, question)
-    return 'Hello World'
-
-'''@app.route('/')
 
 def index():
 
@@ -30,7 +26,7 @@ def index():
 
         return flask.render_template('index.html', question=question, answer=answer)
     else:
-        return flask.render_template('index.html')'''
+        return flask.render_template('index.html')
 
 
 
