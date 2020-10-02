9c6ddcc18e052d897195f6dbb2bbd7856ce2d88a
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-02 21:51:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,28 +9,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World'
+    context = "Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei"
+    question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.get_json(silent=True)
-    result = req.get("queryResult")
-    #user_says=result.get("queryText")
-    #log.write_log(sessionID, "User Says: "+user_says)
-    parameters = result.get("parameters")
-    context=parameters.get("context")
-    question = parameters.get("question")
+    answer = rs.predict(context, question)
+    print(answer)
+    return answer
 
-    
-    if (intent=='QA - yes'):
-        prediction = rs.predict(context,question)
-    
-       
-        fulfillmentText= "The Iris type seems to be..  {} !".format(prediction)
-        #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
-        return {
-            "fulfillmentText": fulfillmentText
-        }
 
 
 if __name__ == "__main__":
