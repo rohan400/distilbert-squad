ba24b8de4b86a7730475ddd42e57b6d2fd1ba658
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-03 10:33:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,10 +32,8 @@ def someHeavyFunction():
 from celery import Celery
 
 app = Flask(__name__)
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-            CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
-#app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-#app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app.config['CELERY_BROKER_URL'] = 'redis://redis:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379/0'
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
