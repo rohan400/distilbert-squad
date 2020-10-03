db72398d9928d33e0bbd09486e28afb99e7c75bd
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-03 15:14:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,26 @@
 import numpy as np
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, Response
 from flask_cors import cross_origin
 import rs
 import os
-'''import threading
-
-
-from concurrent.futures import ThreadPoolExecutor
 from time import sleep
-# DOCS https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
-executor = ThreadPoolExecutor(1)
 
-app = Flask(__name__)
-
+app = Flask('hello')
 
 @app.route('/')
-def run_jobs():
-    future=executor.submit(someHeavyFunction)
-    return_value = future.result()
-    return return_value
-
-
-
-def someHeavyFunction():
+def hello():
     context = "Telefon mobil Samsung Galaxy A31, Dual SIM, 64GB, 4G, Prism Crush Black  Macro Cam surprinde detaliile din apropiere Camera foto macro de 5MP (40 mm) realizeaza fotografii cu claritate si calitate ridicata, ceea ce te ajuta sa scoti in evidenta detaliile foarte fine ale fotografiilor tale, din apropiere. Aplica si ajusteaza estomparea naturala a fundalului (Bokeh) pentru a izola subiectul si a-i creste impactul vizual. Depth Camera iti aduce subiectul in centrul atentiei"
     question = "Ce surprinde Macro Cam la Samsung Galaxy A31?"
-    answer = rs.predict(context, question)
-    print(answer)
-    return answer'''
+    response = Response(rs.predict(context, question))
+    @response.call_on_close
+    def on_close():
+        for i in range(15):
+            sleep(1)
+            print i
+    return response
 
-from celery import Celery
+
+'''from celery import Celery
 
 app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
@@ -50,7 +41,7 @@ def my_background_task():
     rs.load_model()
     answer = rs.predict(context, question)
     print(answer)
-    return answer
+    return answer'''
 
 
 
