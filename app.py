e5c29662df08b82ed9ef336e712944818c6b9918
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-03 17:02:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def hello():
     response = Response(rs.predict(context, question))
     @response.call_on_close
     def on_close():
-        for i in range(15):
+        for i in range(10):
             sleep(1)
             print(i)
     return response
