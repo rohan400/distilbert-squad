5b3628f67249ee13fa5e05ef68def9e3d294a650
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-11 11:48:20 +0200

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import requests
+import os
+
+def download_model(s3_url, model_name):
+    path = "./model"
+    path_to_model = os.path.join(path, model_name)
+    if not os.path.exists(path_to_model):
+        print("Model weights not found, downloading from S3...")
+        print(f"URL:{s3_url}")
+        os.makedirs(os.path.join(path), exist_ok=True)
+        filename = Path(path_to_model)
+        r = requests.get(s3_url)
+        filename.write_bytes(r.content)
+
+    return path_to_model
+
+def load_model():
+
+    s3_model_url = 'https://storage.googleapis.com/bertpepper/pepperqa/pytorch_model.bin'
+
+    path_to_model1 = download_model(s3_model_url, model_name="pytorch_model.bin")
+
+    path_to_model = 'model/pytorch_model.bin
+
+    return path_to_model
+    
+    
+    
