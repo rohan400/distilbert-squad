9354901c67a9e08da97a890e2675068db92734a6
rohan400 <45447445+rohan400@users.noreply.github.com>
2020-10-11 17:13:59 +0200

    Update and rename predict.py to modelNew.py

--- modelNew.py
+++ modelNew.py
@@ -25,6 +25,31 @@ def load_model():
 
    
     return qa_pipeline
+
+
+
+class Model:
+
+    def __init__(self, path: str):
+        self.model = self.model_load(path)
+
+
+    def model_load(self, path):
+
+        s3_model_url = 'https://storage.googleapis.com/bertpepper/pepperqa/pytorch_model.bin'
+        path_to_model = download_model(s3_model_url, model_name="pytorch_model.bin")
+
+        qa_pipeline = pipeline("question-answering",model="model",tokenizer="model")
+
+        return qa_pipeline
+
+    def predict(self, context, question):
+        answer =qa_pipeline({'context': context,'question': question})
+
+
+        return answer
+
+model = Model('model')
     
     
     
